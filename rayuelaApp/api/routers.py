7b76c2c77c0aa076667a494027fb1ba0e87abe5b
tarbz2 <12904993+tarbz2@users.noreply.github.com>
2024-08-19 17:08:11 -0300

    Se quita duplicacion de codigo

--- rayuelaApp/api/routers.py
+++ rayuelaApp/api/routers.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from rayuelaApp.api.viewset import LoginViewSet, RegisterViewSet, ProjectsViewSet, ProjectsWithoutTheUserViewSet
-from rayuelaApp.api.viewset import JoinTheProjectViewSet, CheckinViewset
+from rayuelaApp.api.viewset import JoinDisjoinTheProjectViewSet, CheckinViewset
 
 router = DefaultRouter()
 
@@ -8,8 +8,8 @@ router.register(r'login', LoginViewSet, basename='login')
 router.register(r'register', RegisterViewSet, basename='register')
 router.register(r'projects', ProjectsViewSet, basename='projects')
 router.register(r'projects_diff', ProjectsWithoutTheUserViewSet, basename='projects_diff')
-router.register(r'join_the_project', JoinTheProjectViewSet, basename='join_the_project')
-router.register(r'disjoin_the_project', JoinTheProjectViewSet, basename='disjoin_the_project')
+router.register(r'join_the_project', JoinDisjoinTheProjectViewSet, basename='join_the_project')
+router.register(r'disjoin_the_project', JoinDisjoinTheProjectViewSet, basename='disjoin_the_project')
 router.register(r'checkin', CheckinViewset, basename='checkin')
 
 urlpatterns = router.urls
