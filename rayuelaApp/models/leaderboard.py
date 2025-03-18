5e1e3d08002f52de8ccf2879db438131288d6e87
tar.bz2 <12904993+tarbz2@users.noreply.github.com>
2025-03-18 17:09:01 -0300

    Se quita configuraciones innecesarias en Leaderboard

--- rayuelaApp/models/leaderboard.py
+++ rayuelaApp/models/leaderboard.py
@@ -3,7 +3,7 @@ from django.db import models
 from rayuelaApp.models.project import Project
 
 class Leaderboard(models.Model):
-    order_by_points = models.BooleanField(blank=False, null=False, default=True)
+    order_by_points = models.BooleanField(default=True)
     project = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
 
     class Meta:
